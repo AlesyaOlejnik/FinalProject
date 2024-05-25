51b24293dd148a5e722dc0a6fe5637517455f56a
Evgeniy Pokatashkin <evgeniypokat@gmail.com>
2024-05-26 01:35:31 +0300

    update pom

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,8 +1,10 @@
 package baseEntities;
 
 import com.codeborne.selenide.AssertionMode;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.codeborne.selenide.testng.SoftAsserts;
 import configuration.ReadProperties;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -17,6 +19,8 @@ public class BaseTest {
 
     @BeforeSuite
     public void setupSuite() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = ReadProperties.getUrl();
         Configuration.headless = ReadProperties.isHeadless();
         Configuration.browser = ReadProperties.browserName();
